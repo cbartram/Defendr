262f53b992a8670cf29ffd0127e8cde10ea3f4f8
cbartram <cbartram3@gmail.com>
2020-04-04 17:35:16 -0400

    Added env vars and refactored codebase to be class oriented

--- src/index.js
+++ src/index.js
@@ -1,121 +1,63 @@
-require('dotenv').config();
-const figlet = require('figlet');
-const chalk = require('chalk');
 const request = require('request-promise-native');
-const AWS = require('aws-sdk');
-const EventSource = require('eventsource');
-const express = require('express');
-const {
-    AUTHORIZATION_URL,
-    PORT,
-    CSRF_STATE
-} = require('./constants');
-const {
-    getAccessToken,
-    getRekognitionLabels
-} = require('./util');
+const moment = require('moment');
+const { config } = require('./constants');
+const { NEST_ID } = process.env;
+const Auth = require('./security/Auth');
 
-// Create the Express Server
-const app = express();
+class Nest extends Auth {
 
-// Setup API Version for AWS
-AWS.config.update({
-    region: 'us-east-1',
-    apiVersions: {
-        rekognition: '2016-06-27',
-        s3: '2006-03-01',
-    }
-});
-
-// Create the Kinesis Object
-const rekognition = new AWS.Rekognition();
-
-figlet('Defendr', (err, data) => {
-    console.log(chalk.blue(data));
-    app.listen(PORT, () => {
-        console.log(chalk.green('================================='));
-        console.log(chalk.green(`| Server Listening on port ${PORT} |`));
-        console.log(chalk.green('================================='));
-    });
-});
-
-/**
- * Handles redirecting the user to the Authorization url to show them what permissions are necessary for this
- * app to function correctly
- * @route GET /
- */
-app.get('/', async (req, res) => {
-    console.log(chalk.blue('[INFO] Redirecting to Auth URL: ', AUTHORIZATION_URL));
-    res.redirect(AUTHORIZATION_URL);
-});
-
-/**
- * Handles the oauth callback given from nest
- * @route GET /oauth/callback
- */
-app.get('/oauth/callback', async (req, res) => {
-    const { state, code } = req.query;
-    let accessToken;
-
-    // Check and Validate CSRF State
-    if(state !== CSRF_STATE) {
-        res.status(403).json({
-            success: false,
-            message: 'Invalid CSRF State Query Parameter'
-        });
-
-        return;
+    constructor() {
+        super();
+        this.getOAuthToken();
+        this.getJwtToken();
     }
 
-    try {
-        accessToken = await getAccessToken(code);
-    } catch(err) {
-        console.log(chalk.red('[ERROR] Error trading code for access_token', err.message));
-        res.json({
-            success: false,
-            message: err.message,
-            error: err
-        });
-
-        return;
-    }
-
-    console.log(chalk.blue(`[INFO] Successfully Retrieved access_token:`, accessToken.access_token));
-
-    const headers = {
-        Authorization: `Bearer ${accessToken.access_token}`,
-        'Content-Type': 'text/event-stream'
+    /**
+     * Retrieves a list of recent events that the Nest camera detected. It can take two optional params
+     * start and end which are unix timestamps in seconds since epoch and represent a window of time to retrieve
+     * events for.
+     * @param accessToken String OAuth access token
+     * @param start integer Unix timestamp in seconds representing the starting period of time to retrieve events for
+     * @param end integer Unix timestamp in seconds representing the ending period of time to retrieve events for
+     * @returns {Promise<any>}
+     */
+    async getEvents(accessToken, start = null, end = null) {
+        const options = {
+            'method': 'GET',
+            'url': `${config.urls.NEXUS_HOST}${config.endpoints.EVENTS_ENDPOINT}`,
+            'headers': {
+                'Authorization': `Basic ${accessToken}`
+            }
+        };
+        try {
+            return JSON.parse(await request(options));
+        } catch(e) {
+            console.log('[ERROR] Failed to retrieve events from the Nest API: ', e)
+        }
     };
 
-    const source = new EventSource('https://developer-api.nest.com/devices/cameras/5iDi4p4suzIIh2RTlil0k70ahi5nHAz4BCMuppafqTpc1acgItAvbQ', { headers });
-
-    source.addEventListener('put', async event => {
-        console.log(chalk.blue('[INFO] Nest Streaming Data Received: ', event.data));
-        const  { data } = JSON.parse(event.data);
-        const labels = await getRekognitionLabels(rekognition, data.snapshot_url);
-        res.json({
-            success: true,
-            nest: data,
-            rekognition: labels,
-        });
-    });
-
-
-    source.addEventListener('open', event => {
-        console.log(chalk.blue('[INFO] Nest Streaming Connection opened!'));
-    });
-
-    source.addEventListener('auth_revoked', event => {
-        console.log(chalk.yellow('[WARNING] Authentication token was revoked re-authentication necessary'));
-        // Re-authenticate user
-        res.redirect(AUTHORIZATION_URL);
-    });
-
-    source.addEventListener('error', event => {
-        if (event.readyState == EventSource.CLOSED) {
-            console.log(chalk.yellow('[WARNING] Connection was closed!', event));
-        } else {
-            console.log(chalk.red('[ERROR] An unknown error occurred while streaming events: ', event));
+    /**
+     * Retrieves a single snapshot image and writes it to disk
+     * @param jwt String jwt token
+     * @param id
+     * @returns {Promise<void>}
+     */
+    async getSnapshot(jwt) {
+        const options = {
+            'method': 'GET',
+            'url': `${config.urls.NEXUS_HOST}${config.endpoints.SNAPSHOT_ENDPOINT}${NEST_ID}?crop_type=timeline&width=300`,
+            'headers': {
+                'Authorization': `Basic ${jwt}`
+            }
+        };
+        console.log('[INFO] Fetching Snapshots for URL: ', options.url);
+        try {
+            request(options).pipe(fs.createWriteStream(path.join(__dirname, '..', 'assets', moment().format('YYYY-mm-dd_hh:mm:ss.SSS') + '.jpeg'))).on('close', () => {
+                console.log('[INFO] Done writing image');
+            });
+        } catch(e) {
+            console.log('[ERROR] Failed to retrieve snapshots from the Nest API: ', e)
         }
-    }, false);
-});
+    };
+
+}
---
+++ src/security/Auth.js
@@ -0,0 +1,86 @@
+const fs = require('fs');
+const path = require('path');
+const request = require('request-promise-native');
+const moment = require('moment');
+const { config } = require('../constants');
+const { API_KEY, CLIENT_ID, REFRESH_TOKEN } = process.env;
+
+/**
+ * Auth.js
+ * Manages application security and authentication when interacting with the Nest API.
+ * This class has methods to fetch and retrieve an access token and JWT token
+ */
+class Auth {
+
+    constructor() {
+        this.accessToken = null;
+        this.jwtToken = null;
+    }
+
+    getAccessToken() {
+        return this.accessToken;
+    }
+
+    getJwtToken() {
+        return this.jwtToken;
+    }
+
+    /**
+     * Retrieves a Google OAuth token used to call Nest/Nexus API's and services.
+     * @returns {Promise<any>}
+     */
+    async fetchOAuthToken() {
+        if(this.accessToken) return this.accessToken;
+        const options = {
+            'method': 'POST',
+            'url': config.urls.OAUTH_URL,
+            'headers': {
+                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                'Host': 'oauth2.googleapis.com'
+            },
+            form: {
+                'refresh_token': REFRESH_TOKEN,
+                'client_id': CLIENT_ID,
+                'grant_type': 'refresh_token'
+            }
+        };
+        try {
+            const { access_token } = JSON.parse(await request(options));
+            this.accessToken = access_token;
+            return access_token;
+        } catch(e) {
+            console.log('[ERROR] Failed to retrieve OAuth token from Nest API: ', e);
+        }
+    };
+
+
+    /**
+     * Retrieves a JWT authorization token used to call other Nest/Nexus API's
+     * @param accessToken String OAuth access token from #getOAuthToken function
+     * @returns {Promise<any>}
+     */
+    async fetchJwtToken(accessToken) {
+        if(this.jwtToken) return this.jwtToken;
+        const options = {
+            'method': 'POST',
+            'url': config.urls.JWT_TOKEN_URL,
+            'headers': {
+                'x-goog-api-key': API_KEY,
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Host': 'nestauthproxyservice-pa.googleapis.com',
+                'Authorization': `Bearer ${accessToken}`
+            },
+            body: JSON.stringify({"expire_after":"3600s","policy_id":"authproxy-oauth-policy","google_oauth_access_token":accessToken,"embed_google_oauth_access_token":"true"})
+        };
+        try {
+            const { jwt } = JSON.parse(await request(options));
+            this.jwtToken = jwt;
+            return jwt;
+        } catch(e) {
+            console.log('[ERROR] Failed to retrieve JWT token from Nest API: ', e);
+        }
+    };
+}
+
+module.exports = Auth;
